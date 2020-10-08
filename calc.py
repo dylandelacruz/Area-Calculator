9e770bf951ab355d32dd4af0dc665360cda1ead0
dylandelacruz <71275642+dylandelacruz@users.noreply.github.com>
2020-10-08 07:26:55 -0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,46 @@
+invalid_input = True
+
+def area_calculator():
+    print ("Hello, and welcome!")
+    input_ = input("What shape do you want to calculate the area of?")
+    
+    if input_ == "square":
+        length = int(input("What is the side length?"))
+        area = length ** 2
+
+        print ('The area of the ' + input_ + ' is ' + str(area))
+
+        invalid_input = False
+
+    elif input_ == "rectangle":
+        length = int(input("What is the length?"))
+        width = int(input("What is the width?"))
+        area = length * width
+
+        print ('The area of the ' + input_ + ' is ' + str(area))
+
+        invalid_input = False
+
+    elif input_ == "triangle":
+        base = int(input("What is the base?"))
+        height = int(input("What is the hieght?"))
+        area = base * height / 2
+
+        print ('The area of the ' + input_ + ' is ' + str(area))
+
+        invalid_input = False
+
+    elif input_ == "circle":
+        radius = int(input("What is the radius?"))
+        pi = int(3.14159265359)
+        area = pi * radius ** 2
+
+        print ('The area of the ' + input_ + ' is ' + str(area))
+
+        invalid_inputinput = False
+
+while invalid_input:
+        
+    area_calculator()
+
+    
